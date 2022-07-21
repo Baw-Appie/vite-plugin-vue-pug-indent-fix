7b47a62a52a0431224dea99d9044ff96e2180154
Baw-Appie <pp121324@gmail.com>
2022-07-21 13:29:26 +0900

    v1.0.3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function vitePluginVuePugIndentFix() {
     name: 'vite-plugin-vue-pug-indent-fix',
     transform(src, id) {
       if(/\.(vue)/.test(id)) {
-        const temp = src.match(/^\n?(\s+)/)
+        const temp = src.match(/^\r?\n?(\s+)/)
         const indent = temp && temp[1]
         if (indent) return src.split('\n').map(str => str.replace(indent, '')).join('\n')
         return src
