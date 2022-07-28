565a7340b4fdf4ee02bae183636372e2a8f17a0c
Baw-Appie <pp121324@gmail.com>
2022-07-28 13:19:07 +0900

    Update

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ module.exports = function vitePluginVuePugIndentFix() {
     name: 'vite-plugin-vue-pug-indent-fix',
     transform(src, id) {
       if(/\.(vue)/.test(id)) {
+        if(src.startsWith("<template")) { // Vue3 setup component
+          const contentTemp = src.match(/<template(?:.+?)>(.*?)<\/template>/s)
+          let content = contentTemp && contentTemp[1]
+          const oldContent = content
+          const indentTemp = content.match(/^\r?\n?(\s+)/)
+          const indent = indentTemp && indentTemp[1]
+          if(content && indent) content = content.split('\n').map(str => str.replace(indent, '')).join('\n')
+          return src.replace(oldContent, content)
+        }
         const temp = src.match(/^\r?\n?(\s+)/)
         const indent = temp && temp[1]
         if (indent) return src.split('\n').map(str => str.replace(indent, '')).join('\n')
